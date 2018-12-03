8cf0b910baad0dfde78be1f908dd15f108434b2e
Aamir Khan <akhan@paradromics.com>
2018-12-03 17:06:14 -0600

    hello world

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+hello world
+function d = oxhex2dec(h)
+    tic
+    for k = 1:numel(h)
+        if startsWith(strtrim(h{k}), '0x', 'IgnoreCase',true)
+            h{k} = h{k}(3:end);
+        end
+    end
+    d = hex2dec(h);
+    toc
+end
